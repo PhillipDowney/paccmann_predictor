c8aeee379620e5328f3ac7c1cdcb710a6fccc600
Phil Downey <Phil.downey1@ibm.com>
2024-03-08 08:32:13 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     version=get_version('paccmann_predictor/__init__.py'),
     description=('PyTorch implementation of PaccMann'),
     long_description=open('README.md').read(),
-    url='https://github.com/PaccMann/paccmann_predictor',
+    url='https://github.com/PhillipDowney/paccmann_predictor',
     author='Ali Oskooei, Jannis Born, Matteo Manica, Joris Cadow',
     author_email=(
         'ali.oskooei@gmail.com, jab@zurich.ibm.com, '
